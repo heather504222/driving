187620bd54759eae8d9188d60267c06147d1d72a
Heather <k504222@gmail.com>
2021-04-29 20:48:43 +0800

    add taiwan & USA

---
+++ driving.py
@@ -0,0 +1,14 @@
+age = input('請輸入年齡')
+age = int(age)
+country = input('請輸入國家')
+if country == '台灣':
+	if age >= 18:
+		print('你可以考駕照')
+	else:
+		print('你不可以考駕照')
+elif country == '美國':
+	if age >=16:
+		print('你可以考駕照')
+	else:
+		print('你不可以考駕照')
+		
