02c4b7c9e58afc9cf574ee90c6a41bc1689f2a70
Heather <k504222@gmail.com>
2021-04-29 20:52:58 +0800

    add country limit

--- driving.py
+++ driving.py
@@ -1,6 +1,6 @@
-age = input('請輸入年齡')
+age = input('請輸入年齡:')
 age = int(age)
-country = input('請輸入國家')
+country = input('請輸入國家:')
 if country == '台灣':
 	if age >= 18:
 		print('你可以考駕照')
@@ -11,4 +11,5 @@ elif country == '美國':
 		print('你可以考駕照')
 	else:
 		print('你不可以考駕照')
-		
+else:
+	print('國家只限於台灣及美國')
